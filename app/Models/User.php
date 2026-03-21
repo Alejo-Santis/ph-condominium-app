21537d582986d43dadeb360789057c6d2a00a01d
Alejo-Santis <alejosantismk11@gmail.com>
2026-03-21 18:45:42 -0500

    Feat: priemra iteracion falta refinar muchos campos

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password'          => 'hashed',
             'is_active'         => 'boolean',
-            'last_login_at'     => 'datetimeTz',
+            'last_login_at'     => 'datetime',
         ];
     }
 
